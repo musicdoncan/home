349cb4894d9eea03976a50eea9e48c7624dc47f1
musicdoncan <musicdoncan@gmail.com>
2025-01-11 16:53:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ function changeLanguage(language) {
 
     const selectedContent = document.getElementById(`content-${language}`);
     selectedContent.classList.add('active');
-    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    showContent('it');
+});
+function changeLanguage(language) {
+    document.getElementById('content-it').style.display = 'none';
+    document.getElementById('content-en').style.display = 'none';
+    document.getElementById('content-fr').style.display = 'none';
+    document.getElementById('content-' + language).style.display = 'block';
+}
